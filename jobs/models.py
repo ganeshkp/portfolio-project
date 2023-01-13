57a02dab0d7b3713a2e014aa4887daaf87c8f6ea
ganeshkp <ganeshkumar.patil@gmail.com>
2023-01-13 21:15:53 +0000

    latest changes

--- jobs/models.py
+++ jobs/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 
+
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
-    summary=RichTextField(blank=True, null=True)
+    summary = RichTextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True, )
+
+    class Meta:
+        ordering = ["-timestamp"]
