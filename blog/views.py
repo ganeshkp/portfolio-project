9f57d9fd12f38d3bbb622e6934d44e77f1fe515c
ganeshkp <ganeshkumar.patil@gmail.com>
2023-01-14 21:08:20 +0000

    updated

--- blog/views.py
+++ blog/views.py
@@ -4,18 +4,17 @@ from django.utils import timezone
 
 from .models import Blog
 
-
+#********************************Function based views**********************************#
 # def allblogs(request):
 #     blogs = Blog.objects
 #     return render(request, 'blog/allblogs.html', {'blogs': blogs})
 
+# def detail(request, blog_id):
+#     detailblog = get_object_or_404(Blog, pk=blog_id)
+#     return render(request, 'blog/detail.html', {'blog': detailblog})
 
-def detail(request, blog_id):
-    detailblog = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'blog/detail.html', {'blog': detailblog})
 
-
-# Class based view
+#********************************Class based views**********************************#
 class BlogListView(ListView):
     model = Blog
     # default template name is 'blog/blog_list.html' it file name is <appname>/<modelname>_list.html
