4dcdc603b14e650f97e06c28cb54bbe1a70ae2ef
ganeshkp <ganeshkumar.patil@gmail.com>
2023-01-28 15:36:39 +0000

    updated

--- portfolio/celery.py
+++ portfolio/celery.py
@@ -2,11 +2,30 @@
 
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
-app = Celery("portfolio")
+app = Celery("portfolio")  # portfolio is project name
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'add-every-minute-contrab': {
+        'task': 'multiply_two_numbers',
+        'schedule': crontab(),
+        'args': (16, 16),
+    },
+    'add-every-5-seconds': {
+        'task': 'multiply_two_numbers',
+        'schedule': 5.0,  # in seconds
+        'args': (16, 16)
+    },
+    'add-every-30-seconds': {
+        'task': 'tasks.add',
+        'schedule': 30.0,
+        'args': (16, 16)
+    },
+}
 
 
 @app.task(bind=True)
